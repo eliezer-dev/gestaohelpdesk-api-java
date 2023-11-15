9629003ec86e438faa96e4a5b09bc4d2723191eb
Eliezer Palhão <eliezer.ramosp@gmail.com>
2023-11-15 10:13:05 -0300

    Commit inicial do projeto gerado pelo Spring Initializr

---
+++ src/test/java/dev/eliezer/superticket/SuperticketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eliezer.superticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
